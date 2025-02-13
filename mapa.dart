16a3528ec6ae456053f4cc92c72e2a879fda43f8
Dulce Tolentino <a.22308051281367@cbtis128.edu.mx>
2025-02-13 15:22:31 +0000

    tarea

---
+++ mapa.dart
@@ -0,0 +1,15 @@
+void main(){
+print ("Dulce Maria Tolentino De Anda 2238051281367");
+Map<int, String> alumnos ={
+    1: "Juan";
+    2: "Pedro";
+    3: "Luis ";
+print ("Mapa de alumnos: ");
+ print (alumnos);
+
+print ("iterar un map con forEach");
+ alumnos.forEach((key, value ){
+    print("key: $key, value: $value");
+ });
+}
+}
